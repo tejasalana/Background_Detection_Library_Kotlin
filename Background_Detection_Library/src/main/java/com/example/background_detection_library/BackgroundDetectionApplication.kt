71e27d051874f04c98f580b7bd63bd624dc0bfd2
Teja <salanatejesh@gmail.com>
2020-11-16 16:45:33 +0530

    initial commit

---
+++ Background_Detection_Library/src/main/java/com/example/background_detection_library/BackgroundDetectionApplication.kt
@@ -0,0 +1,26 @@
+package com.example.background_detection_library
+
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
+
+class BackgroundDetectionApplication : Application(), LifecycleObserver{
+
+    override fun onCreate() {
+        super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onForeground(){
+        Toast.makeText(applicationContext,"App Foregrounded", Toast.LENGTH_SHORT).show()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onBackground(){
+        Toast.makeText(applicationContext,"App Backgrounded",Toast.LENGTH_SHORT).show()
+    }
+}
